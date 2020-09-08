4b3380bd5e8c712c94fda463dbadb22c60d484d7
Yeagly <DYeagly@lbl.gov>
2020-09-08 13:10:17 -0700

    added zeiss connection

--- hallprobe_gui.py
+++ hallprobe_gui.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+from zeisscmm import CMM
+from nicdaq import DAQ
 
 class HallProbeApp(tk.Frame):
     def __init__(self, master):
@@ -8,7 +10,7 @@ class HallProbeApp(tk.Frame):
         self.master.title('Hall Probe Integration App')
         self.master.geometry('1200x800')
         self.create_frames()
-        self.pack()
+        self.pack(side='left', padx=30, fill='both')
     
     def create_frames(self):
         self.zeiss_frame = ZeissControls(self)
@@ -20,6 +22,7 @@ class ZeissControls(tk.LabelFrame):
     def __init__(self, parent, title='Zeiss CMM Controls', labelanchor='n'):
         super().__init__(parent, text=title, labelanchor='n')
         self.create_widgets()
+        self.grid(pady=30)
     
     def create_widgets(self):
         self.lbl_ip = tk.Label(self, text='IP Address')
@@ -32,12 +35,31 @@ class ZeissControls(tk.LabelFrame):
         self.ent_port = tk.Entry(self)
         self.ent_port.insert(0, '4712')
         self.ent_port.grid(column=1, row=1)
-        self.btn_connect = tk.Button(self, text='Connect')
+        self.btn_connect = tk.Button(self, text='Connect', command=self.connect_cmm)
         self.btn_connect.grid(column=0, row=2)
-        self.btn_disconnect = tk.Button(self, text='Disconnect')
+        self.btn_disconnect = tk.Button(self, text='Disconnect', command=self.disconnect_cmm)
         self.btn_disconnect.grid(column=1, row=2)
         self.lbl_conn_status = tk.Label(self, text='*Connection Status*')
         self.lbl_conn_status.grid(column=0, row=3, columnspan=2)
+
+    def connect_cmm(self):
+        try:
+            self.zeiss = CMM(ip=self.ent_ip.get(), port=int(self.ent_port.get()))
+            self.btn_connect['state'] = 'disabled'
+            self.lbl_conn_status['text'] = 'Connection Established'
+        except ConnectionRefusedError:
+            self.lbl_conn_status['text'] = 'Connection Refused'
+        except TimeoutError:
+            self.lbl_conn_status['text'] = 'Connection Timed Out'
+
+    def disconnect_cmm(self):
+        try:
+            self.zeiss.close()
+            self.btn_connect['state'] = 'normal'
+            self.lbl_conn_status['text'] = 'Disconnected'
+        except AttributeError:
+            self.lbl_conn_status['text'] = 'Already Disconnected'
+
 
 class HallProbe(tk.LabelFrame):
     def __init__(self, parent, title='Hall Probe Controls'):
