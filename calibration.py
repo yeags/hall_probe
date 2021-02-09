a8cd5c6912da66ac16214155d22a9a9c429478cd
Dave Yeagly <dave@yeags.com>
2021-02-09 15:07:39 -0800

    minor updates

--- calibration.py
+++ calibration.py
@@ -2,7 +2,12 @@ import numpy as np
 import re
 import os
 
-def clean_raw_data(sensor_raw, outlier_factor = 3):
+def clean_raw_data(sensor_raw, outlier_factor = 2):
+    '''
+    sensor_raw is an (n, 4) numpy array (Bx, By, Bz, Temp_out) of raw analog input reading
+    raw_filt detects outliers and replaces them with their respective mean value
+    function returns a (4,) numpy array of recalculated mean values
+    '''
     raw_mean = np.mean(sensor_raw, axis=0)
     raw_stdev = np.std(sensor_raw, axis=0, ddof=1)
     raw_filt = (sensor_raw > -outlier_factor*raw_stdev+raw_mean) & (sensor_raw < outlier_factor*raw_stdev+raw_mean)
@@ -29,21 +34,29 @@ def get_xyz_calib_values(path: str):
     xyz_coeffs = np.array([i for i in coeffs_dict.values()])
     return xyz_coeffs
 
-def calib_data(calib_coeffs, sensor_data, sensitivity=5.0):
+def calib_data(calib_coeffs, sensor_data, sensitivity=5):
     '''
-    Arguments:  calib_coeffs is a (3,3,7) numpy array of calibration coefficients
-                sensitivity is the volts per tesla of the probe
-                sensor_data should be a (4,) numpy array (Bx, By, Bz, Temperature(in volts))
+    Arguments:
+        calib_coeffs is a (3,3,7) numpy array of calibration coefficients
+        sensitivity is the volts per tesla of the probe.  Use only either 5 (2 T range) or 100 (100 mT range)
+        default range is 2 T
+        sensor_data should be a (4,) numpy array (Bx, By, Bz, Temperature(in volts))
+        sensor_data can either be cleaned or raw data.  Must have shape (n, 4) for raw data.
+    Returns:
+        function returns calibrated hall sensor readings (Bx,By,Bz)
     '''
+    if sensor_data.shape != (4,):
+        sensor_data = clean_raw_data(sensor_data)
+
     Bxyz = sensor_data[:3]
     temp_v = calib_coeffs[0, 0, 6]*sensor_data[3] + calib_coeffs[0, 0, 5]
-    if sensitivity == 5.0:
+    if sensitivity == 5:
         k1 = calib_coeffs[:, 2, 0]
         k2 = calib_coeffs[:, 2, 1]
         k3 = calib_coeffs[:, 2, 2]
         k4 = calib_coeffs[:, 2, 3]
         k5 = calib_coeffs[:, 2, 4]
-    elif sensitivity == 100.0:
+    elif sensitivity == 100:
         k1 = calib_coeffs[:, 0, 0]
         k2 = calib_coeffs[:, 0, 1]
         k3 = calib_coeffs[:, 0, 2]
@@ -55,12 +68,6 @@ def calib_data(calib_coeffs, sensor_data, sensitivity=5.0):
     xyz_dbl_prime = k2 * xyz_prime**3 + xyz_prime
     xyz_cal_mT = (2 * (k5 * (xyz_dbl_prime + xyz_dbl_prime * k3 * temp_v + k4 * temp_v)) / sensitivity) * 1000
     return xyz_cal_mT
-
-
-def get_calib_temp(temp_out, gain_temp, temp_offset):
-    temp_v = gain_temp*temp_out + temp_offset
-    return temp_v
-
 
 if __name__ == "__main__":
     path = 'C:/Users/dyeagly/Documents/hall_probe/hall_probe/Hall probe 443-20'
